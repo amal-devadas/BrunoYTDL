2159c88d74328a7075bd0bc58a939b60afac11eb
amal-devadas <49014733+amal-devadas@users.noreply.github.com>
2020-07-10 20:50:24 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -45,7 +45,6 @@ Expires on: 31/12/2020"""
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
 
 --------
 Send /me to know current plan details
